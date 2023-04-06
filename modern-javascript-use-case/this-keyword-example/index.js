8a89d24e9ef9bb80e8953bdf26c36e006df05a54
Çağrı Demirtaş <49253914+cagridemirtash@users.noreply.github.com>
2023-04-06 16:40:53 +0300

    Modern JavaScript This Exampled Added

---
+++ modern-javascript-use-case/this-keyword-example/index.js
@@ -0,0 +1,28 @@
+class NameField {
+  constructor(name) {
+    const field = document.createElement("li");
+    field.textContent = name;
+    const nameListHook = document.querySelector("#names");
+    nameListHook.appendChild(field);
+  }
+}
+
+class NameGenerator {
+  constructor() {
+    const btn = document.querySelector("button");
+    this.names = ["Çağrı", "Beyza", "Deniz", "Ömer"];
+    this.currentNameIndex = 0;
+    btn.addEventListener("click", this.addName.bind(this));
+  }
+
+  addName() {
+    console.log(this);
+    const name = new NameField(this.names[this.currentNameIndex]);
+    this.currentNameIndex++;
+    if (this.currentNameIndex >= this.names.length) {
+      this.currentNameIndex = 0;
+    }
+  }
+}
+
+new NameGenerator();
